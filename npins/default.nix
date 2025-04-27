deb0c739148c3721f84e3d56bd8099552211629b
cinereal <cinereal@riseup.net>
2025-04-27 20:42:18 +0200

    patch npins to handle branches
    see https://github.com/andir/npins/issues/96

--- npins/default.nix
+++ npins/default.nix
@@ -99,12 +99,16 @@ let
             if matched == null then "source" else builtins.head matched
           }${appendShort}";
         name = urlToName url revision;
-      in builtins.fetchGit {
-        rev = revision;
-        inherit name;
-        # hash = hash;
-        inherit url submodules;
-      };
+      in
+      builtins.fetchGit (
+        {
+          rev = revision;
+          inherit name;
+          # hash = hash;
+          inherit url submodules;
+        }
+        // (if builtins.isNull branch then { } else { ref = branch; })
+      );
 
   mkPyPiSource = { url, hash, ... }:
     builtins.fetchurl {
