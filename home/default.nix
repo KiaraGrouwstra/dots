a2533fe83e9a8cefebfe0c7324ecb4bda76de47d
cinereal <cinereal@riseup.net>
2025-05-10 05:44:04 +0200

    pay-respects

--- home/default.nix
+++ home/default.nix
@@ -40,6 +40,7 @@
         enableNushellIntegration = true;
         useTheme = "catppuccin";
       };
+      pay-respects.enable = true;
       yazi.enable = true;
       chromium.enable = true;
     };
