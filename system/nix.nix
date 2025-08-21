c35eb0ae024500d748f4147651f8bce53019e306
cinereal <cinereal@riseup.net>
2025-08-21 13:04:27 +0200

    trusted-users

--- system/nix.nix
+++ system/nix.nix
@@ -58,7 +58,7 @@
   nix = {
     settings = {
       experimental-features = "nix-command flakes";
-      trusted-users = [ "@wheel" ];
+      trusted-users = [ "root" "@wheel" "kiara" ];
       # use a placeholder where we want our secret substituted in
       access-tokens = ''
         github.com=${config.vars.generators."prompted".files."github-pat".placeholder}
