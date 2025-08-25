d39af82df9f2b6b63bdfcb501f64176e1c0e52fb
cinereal <cinereal@riseup.net>
2025-08-25 10:40:18 +0200

    nushell notifications

--- home/nushell.nix
+++ home/nushell.nix
@@ -3,6 +3,7 @@
   config,
   lib,
   pkgs,
+  sources,
   ...
 }:
 {
@@ -19,12 +20,20 @@
           $env.config.show_banner = false
           $env.config.buffer_editor = "hx"
           $env.config.hooks.command_not_found = source ${./command-not-found.nu}
+          $env.config.hooks.pre_prompt = [{||
+            let elapsed = $env.CMD_DURATION_MS | into int | into duration -u ms
+            if $elapsed >= 5sec {
+              let body = $"Task completed in ($elapsed)"
+              notify -s "Task Finished" -t $body
+            }
+          }]
         '';
       plugins = lib.attrValues {
         inherit (pkgs.nushellPlugins)
           gstat
           polars
           ;
+        desktop_notifications = (pkgs.callPackage "${sources.nushell-notifications}/pkgs/shells/nushell/plugins/desktop_notifications.nix" { });
       };
       shellAliases = {
         jq = "jaq";
