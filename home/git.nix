8abfbc422a55859b06c5a033a1adbf346c5bae43
cinereal <cinereal@riseup.net>
2025-12-13 12:23:12 +0100

    migrate git settings
    Signed-off-by: cinereal <cinereal@riseup.net>

--- home/git.nix
+++ home/git.nix
@@ -5,29 +5,30 @@
 {
   _class = "homeManager";
 
+  programs.delta = {
+    enable = true;
+    enableGitIntegration = true;
+    options = {
+      decorations = {
+        commit-decoration-style = "bold yellow box ul";
+        file-decoration-style = "none";
+        file-style = "bold yellow ul";
+      };
+      features = "decorations";
+      whitespace-error-style = "22 reverse";
+    };
+  };
+
   programs.git = {
     enable = true;
     lfs.enable = true;
-    userName = "cinereal";
-    userEmail = "cinereal@riseup.net";
-    delta = {
-      enable = true;
-      options = {
-        decorations = {
-          commit-decoration-style = "bold yellow box ul";
-          file-decoration-style = "none";
-          file-style = "bold yellow ul";
-        };
-        features = "decorations";
-        whitespace-error-style = "22 reverse";
+    settings = {
+      user.name = "cinereal";
+      user.email = "cinereal@riseup.net";
+      alias = {
+        # commit staged changes to main branch
+        main = "!export BRANCH=$(git rev-parse --abbrev-ref HEAD) && git stash --keep-index --include-untracked && git switch main && git commit && git push && git switch $BRANCH && git rebase main && git push --force && git stash pop";
       };
-    };
-    aliases = {
-      # commit staged changes to main branch
-      main = "!export BRANCH=$(git rev-parse --abbrev-ref HEAD) && git stash --keep-index --include-untracked && git switch main && git commit && git push && git switch $BRANCH && git rebase main && git push --force && git stash pop";
-    };
-
-    extraConfig = {
       core.editor = "$EDITOR";
       init.defaultBranch = "main";
       advice.objectNameWarning = false;
