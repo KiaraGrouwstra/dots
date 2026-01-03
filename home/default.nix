b78cd16f5d3b1512f9b40d996b5eea6c358d8886
Kiara Grouwstra <cinereal@riseup.net>
2026-01-03 15:12:05 +0100

    mime

--- home/default.nix
+++ home/default.nix
@@ -6,38 +6,53 @@
 {
   _class = "nixos";
 
-  home-manager.users.${user} = {
-    _class = "homeManager";
+  home-manager.users.${user} =
+    { config, ... }:
+    {
+      _class = "homeManager";
 
-    imports = [
-      ./dotfiles.nix
-      ./git.nix
-      ./helix.nix
-      ./lazygit.nix
-      ./nushell.nix
-      ./style.nix
-      ./wezterm.nix
-      ./neovim
-    ];
-    home.stateVersion = "24.11";
-    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    programs = {
-      firefox = {
+      imports = [
+        ./dotfiles.nix
+        ./git.nix
+        ./helix.nix
+        ./lazygit.nix
+        ./nushell.nix
+        ./style.nix
+        ./wezterm.nix
+        ./neovim
+      ];
+      home.stateVersion = "24.11";
+      dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      xdg.mimeApps = {
         enable = true;
-        nativeMessagingHosts = [ pkgs.keepassxc ];
+        defaultApplicationPackages = [
+          pkgs.zathura
+          config.programs.firefox.package
+          pkgs.yazi
+          pkgs.xfce.thunar
+          config.programs.helix.package
+          config.programs.neovim.package
+          pkgs.libreoffice
+          pkgs.vlc
+        ];
       };
-      oh-my-posh = {
-        enable = true;
-        enableNushellIntegration = true;
-        useTheme = "catppuccin";
+      programs = {
+        firefox = {
+          enable = true;
+          nativeMessagingHosts = [ pkgs.keepassxc ];
+        };
+        oh-my-posh = {
+          enable = true;
+          enableNushellIntegration = true;
+          useTheme = "catppuccin";
+        };
+        tealdeer.enable = true;
+        pay-respects = {
+          enable = true;
+          enableNushellIntegration = true;
+        };
+        yazi.enable = true;
+        chromium.enable = true;
       };
-      tealdeer.enable = true;
-      pay-respects = {
-        enable = true;
-        enableNushellIntegration = true;
-      };
-      yazi.enable = true;
-      chromium.enable = true;
     };
-  };
 }
