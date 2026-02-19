7723c92ddf3e2d33e26e50a90cb325ea42c84461
cinereal <cinereal@riseup.net>
2026-02-19 18:52:15 +0100

    update nixos-configuration
    Signed-off-by: cinereal <cinereal@riseup.net>

--- system/default.nix
+++ system/default.nix
@@ -12,7 +12,7 @@ let
     let
       entries = lib.mapAttrsToList (k: v: k + "=" + v) sources;
     in
-    "${lib.concatStringsSep ":" entries}:nixos-config=/home/${user}/nixos/configuration.nix:flake=${pkgs.path}:flake";
+    "${lib.concatStringsSep ":" entries}:nixos-config=/etc/nixos/configuration.nix:flake=${pkgs.path}:flake";
   specialArgs = {
     inherit
       sources
