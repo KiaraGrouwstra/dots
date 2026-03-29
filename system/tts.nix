b02717cf3176d6b9d5db74d06446a2f019745bda
cinereal <cinereal@riseup.net>
2026-03-29 23:24:59 +0200

    TTS: nl
    Signed-off-by: cinereal <cinereal@riseup.net>

--- system/tts.nix
+++ system/tts.nix
@@ -1,9 +1,11 @@
 { pkgs, sources, ... }:
 let
-  amyVoice = pkgs.runCommand "piper-voice-en_US-amy-medium" { } ''
+  voicesDir = pkgs.runCommand "piper-voices" { } ''
     mkdir -p $out
     cp ${sources."piper-amy-onnx"} $out/en_US-amy-medium.onnx
     cp ${sources."piper-amy-json"} $out/en_US-amy-medium.onnx.json
+    cp ${sources."piper-pim-onnx"} $out/nl_NL-pim-medium.onnx
+    cp ${sources."piper-pim-json"} $out/nl_NL-pim-medium.onnx.json
   '';
 in
 {
@@ -26,7 +28,7 @@ in
     '';
     modules.piper-generic = ''
       GenericExecuteSynth \
-      "printf %s \'$DATA\' | ${pkgs.piper-tts}/bin/piper --model ${amyVoice}/en_US-amy-medium.onnx --output-raw | ${pkgs.alsa-utils}/bin/aplay -r 22050 -f S16_LE -c 1 -t raw - 2>/dev/null"
+      "printf %s \'$DATA\' | ${pkgs.piper-tts}/bin/piper --model ${voicesDir}/$VOICE.onnx --output-raw | ${pkgs.alsa-utils}/bin/aplay -r 22050 -f S16_LE -c 1 -t raw - 2>/dev/null"
 
       GenericCmdDependency "${pkgs.piper-tts}/bin/piper"
       GenericSoundIconFolder "/usr/share/sounds/sound-icons/"
@@ -37,6 +39,7 @@ in
       GenericPunctAll ""
 
       AddVoice "en" "FEMALE1" "en_US-amy-medium"
+      AddVoice "nl" "MALE1" "nl_NL-pim-medium"
       DefaultVoice "en_US-amy-medium"
     '';
   };
